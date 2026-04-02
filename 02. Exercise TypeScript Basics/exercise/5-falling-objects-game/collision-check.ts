c57e25c0e5936a80f1e7259b142da2568d32324e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-04-02 12:55:16 +0300

    collision check

---
+++ 02. Exercise TypeScript Basics/exercise/5-falling-objects-game/collision-check.ts
@@ -0,0 +1,12 @@
+import { Bounds } from "./interfaces";
+
+function checkCollision(a: Bounds, b: Bounds): boolean {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
+
+export default checkCollision;
